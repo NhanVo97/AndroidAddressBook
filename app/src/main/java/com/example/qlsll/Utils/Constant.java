9f97ac0904777bb3d4239cc6281b9435fc593864
sang <sang0968991331@gmail.com>
2019-04-05 18:05:59 +0700

    update

--- app/src/main/java/com/example/qlsll/Utils/Constant.java
+++ app/src/main/java/com/example/qlsll/Utils/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
     // ID ADMIN
     public static final String ADMIN_ID = "Admin";
     // API
-    public static final String BASE_URL_API = "http://192.168.42.130:8080/api/v1/";
+    public static final String BASE_URL_API = "http://192.168.43.152:8080/api/v1/";
     // Time Toast
     public static final int TOASTSORT = 0;
     public static final int TOASTLONG = 1;
