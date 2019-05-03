d0180808a0ecaaf3cca03a8e2fae90ffc65ae1b8
Lee Sang <sang0968991331@gmail.com>
2019-05-03 19:53:35 +0700

    update

--- app/src/main/java/com/example/qlsll/Utils/Constant.java
+++ app/src/main/java/com/example/qlsll/Utils/Constant.java
@@ -4,11 +4,9 @@ public class Constant {
     // ID ADMIN
     public static final String ADMIN_ID = "Admin";
     // API
-<<<<<<< HEAD
+
     public static final String BASE_URL_API = "http://192.168.43.12:8083/api/v1/";
-=======
-    public static final String BASE_URL_API = "http://192.168.1.147:8080/api/v1/";
->>>>>>> 781c6110084531dc005b6ca89ca5f8d5f12cf2e6
+
     // Time Toast
     public static final int TOASTSORT = 0;
     public static final int TOASTLONG = 1;
